f3c010f8a9b50fdee9cea575c2a7d5a561b6f4b7
CodeRiverside <coderiversidecr@gmail.com>
2025-10-04 13:44:30 -0400

    @RestControllerAdvice

--- src/main/java/com/coderiverside/quicknote/exception/GlobalExceptionHandler.java
+++ src/main/java/com/coderiverside/quicknote/exception/GlobalExceptionHandler.java
@@ -4,12 +4,12 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NoteNotFoundException.class)
